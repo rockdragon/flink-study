5b4e31b8dfe3428bbbb332300ed4541698c92b7b
moye <moye@daypop.ai>
2020-09-16 15:14:41 +0800

    feat: add sink to Kafka

--- src/main/scala/me/moye/StreamingJob.scala
+++ src/main/scala/me/moye/StreamingJob.scala
@@ -20,6 +20,10 @@ package me.moye
 
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.time.Time
+import java.util.Properties
+
+import org.apache.flink.streaming.api.functions.sink.SinkFunction
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
 /**
  * Skeleton for a Flink Streaming Job.
@@ -47,30 +51,28 @@ object StreamingJob {
       .timeWindow(Time.seconds(5))
       .sum(1)
 
-    windowWordCount.print().setParallelism(1)
-    env.execute("Socket Window WordCount")
-
-    /*
-     * Here, you can start creating your execution plan for Flink.
-     *
-     * Start with getting some data from the environment, like
-     *  env.readTextFile(textPath);
-     *
-     * then, transform the resulting DataStream[String] using operations
-     * like
-     *   .filter()
-     *   .flatMap()
-     *   .join()
-     *   .group()
-     *
-     * and many more.
-     * Have a look at the programming guide:
-     *
-     * http://flink.apache.org/docs/latest/apis/streaming/index.html
-     *
-     */
+    windowWordCount
+      .addSink(new SinkFunction[(String, Int)] {
+        override def invoke(in: (String, Int)): Unit = {
+          sendMessage("(%s,%d)".format(in._1, in._2))
+        }
+      })
+      .setParallelism(1)
 
     // execute program
-    env.execute("Flink Streaming Scala API Skeleton")
+    env.execute("Socket Window WordCount")
+  }
+
+  def sendMessage(message: String): Unit = {
+    val props = new Properties()
+    props.put("bootstrap.servers", "10.129.15.179:9092,10.129.15.229:9092,10.129.15.76:9092")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    val topic = "test_topic"
+    val producer = new KafkaProducer[String, String](props)
+    val jsonData = "{\"message\": \"%s\"}".format(message)
+    val data = new ProducerRecord[String, String](topic, jsonData)
+    producer.send(data)
+    producer.close()
   }
 }
